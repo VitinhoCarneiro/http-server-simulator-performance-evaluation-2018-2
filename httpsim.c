0fad74816edc641f3d0a51dee8b85d9a2ff86e15
bitinhokaroneero <victorcarneiro2009@gmail.com>
2018-11-29 22:36:39 -0200

    Added skeleton for main program

--- httpsim.c
+++ httpsim.c
@@ -8,8 +8,23 @@
 #include <stdio.h>
 #endif
 
+#include "queue.h"
+#include "random_var.h"
+
 //int main (int argc, char** argv)
 int main ()
 {
+	float current_time;
+
+	/* TODO: Read these from arguments */
+	uint32_t request_queue_length_max = 50;
+	float request_mean_delay = 30.f;
+	float mean_service_time = 20.f;
+	float max_simulation_time = 5000.f;
+
+	queue request_queue;
+
+	
+
 	return 0;
 }
